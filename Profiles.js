1c62ce236fb541194ffde419c99d19b8c9a313d1
Aswin Sreekumar <88987196+Aswin-Sk@users.noreply.github.com>
2023-07-03 17:59:24 +0530

    Update Profiles.js

--- Profiles.js
+++ Profiles.js
@@ -2,11 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Plot from 'react-plotly.js';
 
-
 const Profiles = () => {
-  console.log("Logging from Profiles component"); 
+  console.log("Logging from Profiles component");
   const [profiles, setProfiles] = useState([]);
   const [tickers, setTickers] = useState([]);
+  const [loading, setLoading] = useState(false); // New state for loading indicator
+
+  // Fetch profiles on component mount
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
+
+  // Fetch tickers on component mount
+  useEffect(() => {
+    fetchTickers();
+  }, []);
+
+  // Fetch profiles from API
   const fetchProfiles = async () => {
     try {
       const response = await axios.get('http://localhost:8080/profile');
@@ -16,50 +28,46 @@ const Profiles = () => {
       alert('Cannot fetch profiles');
     }
   };
-  useEffect(() => {
-    fetchProfiles();
-  }, []);
 
-  useEffect(() => {
-    const parseTickerSymbols = (data) => {
-      const tickerSymbols = [];
-      const rows = data.split('\n');
-      const headers = rows[0].split(',');
-      const symbolIndex = headers.indexOf('symbol');
-      for (let i = 1; i < rows.length; i++) {
-        const row = rows[i].split(',');
-        if (row.length > symbolIndex) {
-          const ticker = row[symbolIndex].trim();
-          tickerSymbols.push(ticker);
-        }
+  // Parse ticker symbols from AlphaVantage response
+  const parseTickerSymbols = (data) => {
+    const tickerSymbols = [];
+    const rows = data.split('\n');
+    const headers = rows[0].split(',');
+    const symbolIndex = headers.indexOf('symbol');
+    for (let i = 1; i < rows.length; i++) {
+      const row = rows[i].split(',');
+      if (row.length > symbolIndex) {
+        const ticker = row[symbolIndex].trim();
+        tickerSymbols.push(ticker);
       }
-    
-      return tickerSymbols;
-    };
-    const fetchTickers = async () => {
-      try {
-        const response = await axios.get(
-          'https://www.alphavantage.co/query?function=LISTING_STATUS&state=active&apikey= 47N9UD8M7YMFVEO1'
-        );
-        const tickerSymbols = parseTickerSymbols(response.data);
-        setTickers(tickerSymbols);
-      } catch (error) {
-        console.error(error);
-        alert('Cannot fetch tickers');
-      }
-    };
-    fetchTickers();
-  }, []);
+    }
+    return tickerSymbols;
+  };
 
- 
+  // Fetch tickers from AlphaVantage API
+  const fetchTickers = async () => {
+    try {
+      const response = await axios.get(
+        'https://www.alphavantage.co/query?function=LISTING_STATUS&state=active&apikey=47N9UD8M7YMFVEO1'
+      );
+      const tickerSymbols = parseTickerSymbols(response.data);
+      setTickers(tickerSymbols);
+    } catch (error) {
+      console.error(error);
+      alert('Cannot fetch tickers');
+    }
+  };
+
+  // Create a new profile
   const createProfile = async () => {
     try {
       const newProfileId = generateUniqueId();
       const newProfile = {
         id: newProfileId,
         data: {
-          ticker: ''
-        }
+          ticker: '',
+        },
       };
       await axios.post('http://localhost:8080/profile', newProfile);
       fetchProfiles();
@@ -68,6 +76,8 @@ const Profiles = () => {
       alert('Error creating profile. Please try again.');
     }
   };
+
+  // Delete a profile
   const deleteProfile = async (id) => {
     try {
       await axios.delete(`http://localhost:8080/profile/${id}`);
@@ -77,27 +87,31 @@ const Profiles = () => {
       alert('Error deleting profile. Please try again.');
     }
   };
+
+  // Call Python API
   const callPythonAPI = async (request) => {
     try {
       const jsonRequest = JSON.stringify(request);
       return axios.post('http://localhost:5000/api/main', jsonRequest, {
         headers: {
-          'Content-Type': 'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
       });
     } catch (error) {
       console.error('Error calling Python API:', error);
     }
   };
+
+  // Update ticker and fetch updated data
   const updateTicker = async (id, ticker) => {
     try {
       const profileToUpdate = profiles.find((profile) => profile.id === id);
-      const request={
-        company_name:ticker,
-        predict:false
-      }
-      
-      const response=await callPythonAPI(request);
+      const request = {
+        company_name: ticker,
+        predict: false,
+      };
+
+      const response = await callPythonAPI(request);
       profileToUpdate.data.ticker = ticker;
       profileToUpdate.data.plot = response.data.plot;
       profileToUpdate.data.risk_output = response.data.risk_output;
@@ -108,6 +122,8 @@ const Profiles = () => {
       alert('Error updating ticker. Please try again.');
     }
   };
+
+  // Parse plot data from string to JSON object
   const parsePlotData = (plotData) => {
     try {
       if (!plotData) {
@@ -125,8 +141,8 @@ const Profiles = () => {
     }
     return null; // or return a fallback value
   };
-  
-  
+
+  // Generate a unique ID for new profiles
   const generateUniqueId = () => {
     if (profiles.length === 0) {
       return 1;
@@ -134,6 +150,29 @@ const Profiles = () => {
 
     const maxId = Math.max(...profiles.map((profile) => profile.id));
     return maxId >= 1 ? maxId + 1 : 1;
+  };
+
+  // Call Python API with predict set to true
+  const predictAndPlot = async (id) => {
+    try {
+      setLoading(true);
+      const profileToUpdate = profiles.find((profile) => profile.id === id);
+      const request = {
+        company_name: profileToUpdate.data.ticker,
+        predict: true,
+      };
+
+      const response = await callPythonAPI(request);
+      profileToUpdate.data.plot = response.data.plot;
+      profileToUpdate.data.risk_output = response.data.risk_output;
+      await axios.put(`http://localhost:8080/profile`, profileToUpdate);
+      await fetchProfiles();
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      alert('Error predicting and plotting. Please try again.');
+    }
   };
 
   return (
@@ -146,7 +185,14 @@ const Profiles = () => {
             <p>ID: {profile.id}</p>
             {/* Display other profile properties */}
             {profile.data.ticker ? (
-              <p>Ticker: {profile.data.ticker}</p>
+              <>
+                <p>Ticker: {profile.data.ticker}</p>
+                {loading ? (
+                  <p>Generating plot...</p>
+                ) : (
+                  <button onClick={() => predictAndPlot(profile.id)}>Predict using AI</button>
+                )}
+              </>
             ) : (
               <select
                 value={profile.data.ticker}
